352112d370a60e3da290f689b808de2bfa2edc1c
Dron245 <Dron245@yandex.ru>
2024-09-12 22:09:43 +0300

    1209-2209

---
+++ src/js/files/script.js
@@ -0,0 +1,11 @@
+// Подключение функционала "Чертоги Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+// Подключение галереи Fancybox
+Fancybox.bind('[data-fancybox]', {
+	//
+ });
